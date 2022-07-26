d271630934a4e7d3bd447fd7db9d602dbf4d5679
Danh Tran <danh2k@nexondv.com>
2022-07-26 11:49:47 +0700

    delete unused

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ const urlDoc = "https://hupedu-my.sharepoint.com/:b:/g/personal/tt_adr_hup_edu_v
 querySelectStr = (pageNum) => `canvas[aria-label='Page ${pageNum}']`;
 // const selectorPageNav = "div[data-bind='text:ofPagesLabel']";
 
-const convertBase64ToBlob = (base64) => {
-	const parts = base64.split(";base64,");
-	const imageType = parts[0].split(":")[1];
-	const decodedData = window.atob(parts[1]);
-	const uInt8Array = new Uint8Array(decodedData.length);
-	for (let i = 0; i < decodedData.length; ++i) {
-		uInt8Array[i] = decodedData.charCodeAt(i);
-	}
-	return new Blob([uInt8Array], { type: imageType });
-};
-
-const getFile = (b64data) => {
-	try {
-		const blob = convertBase64ToBlob(b64data);
-		const fileURL = URL.createObjectURL(blob);
-		return fileURL;
-	} catch (error) {
-		console.error(error);
-		return null;
-		a;
-	}
-};
-
 (async () => {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
